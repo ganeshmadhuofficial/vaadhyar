78bfe5699b9a07f4ba5110a9ddd8593aca1bfb3a
Ganesh Maduranathan <gmaduranathan@gmail.com>
2016-07-19 20:44:53 +0530

    update and delete users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :null_session
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: user_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: user_keys)
+  end
+
+  def user_keys
+    [:email, :password, :password_confirmation, :first_name, :last_name, :phone_number, :age, :experience, :is_admin, :is_vaadhyar, :state, :city, :region, :latitude, :longitude, :vedam => [], :classification => []]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, :rememberable
   include DeviseTokenAuth::Concerns::User
 
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
   validates :phone_number, presence: true, uniqueness: true
   validates :age, presence: true
   validates :experience, presence: true
