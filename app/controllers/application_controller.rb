b3b7991927c8a27a6357a51213cbdd78800ed02b
Ganesh Maduranathan <gmaduranathan@gmail.com>
2016-07-20 22:15:55 +0530

    Revert "permitted params"
    This reverts commit e4990223bbca93974e2c6f8ce449dd68e4faed55.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
-  prepend_before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
